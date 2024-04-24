875c4d7790c8da7525b8eb927c51d2f604416197
Mueem-Nahid <mueem51@gmail.com>
2024-04-24 10:46:06 +0600

    # Feature - add text

--- src/pages/StartAnalysisPage.tsx
+++ src/pages/StartAnalysisPage.tsx
@@ -1,14 +1,34 @@
 import {useState} from "react";
+import {useAddTextMutation} from "../redux/features/analyzer/analyzerApi.ts";
+import {toast} from "react-toastify";
 
 const StartAnalysisPage = () => {
   const [text, setText] = useState<string>("");
 
-  const handleSubmit = (e: { preventDefault: () => void; }) => {
+  const [addText, {isLoading}] = useAddTextMutation();
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
     e.preventDefault();
+    try {
+      const res = await addText({text});
+      console.log("res: ", res);
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      if (res?.data?.success) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        toast.success(res.data?.message);
+        setText("");
+      } else {
+        toast.error("Something went wrong");
+      }
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   return (
-    <div className="mx-auto max-w-2xl">
+    <div className="mx-auto max-w-2xl mt-20">
       <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-xl">
         Write a few sentences to start analysis
       </h1>
@@ -18,13 +38,15 @@ const StartAnalysisPage = () => {
             <textarea
               id="text"
               name="text"
+              required
               onChange={(e) => setText(e.target.value)}
               rows={10}
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               value={text}
             />
             <button type="submit"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit
+                    className="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              {isLoading ? "Submitting" : "Submit"}
             </button>
           </form>
         </div>
---
+++ src/redux/features/analyzer/analyzerApi.ts
@@ -0,0 +1,60 @@
+import {api} from "../../api/apiSlice.ts";
+
+const analyzerApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    singleText: builder.query({
+      query: (id: string) => `/analyzer/${id}`,
+    }),
+    countWords: builder.query({
+      query: (id: string) => `/analyzer/${id}/words`,
+    }),
+    countCharacters: builder.query({
+      query: (id: string) => `/analyzer/${id}/characters`,
+    }),
+    countParagraphs: builder.query({
+      query: (id: string) => `/analyzer/${id}/paragraphs`,
+    }),
+    countSentences: builder.query({
+      query: (id: string) => `/analyzer/${id}/sentences`,
+    }),
+    getLongestWords: builder.query({
+      query: (id: string) => `/analyzer/${id}/longest-words`,
+    }),
+    allText: builder.query({
+      query: () => `/analyzer`,
+    }),
+    addText: builder.mutation({
+      query: (payload) => ({
+        url: '/analyzer',
+        method: 'POST',
+        body: payload
+      }),
+    }),
+    updateText: builder.mutation({
+      query: ({id, updatedData}) => ({
+        url: `/analyzer/${id}`,
+        method: 'PATCH',
+        body: updatedData
+      }),
+    }),
+    deleteText: builder.mutation({
+      query: (id) => ({
+        url: `/analyzer/${id}`,
+        method: 'DELETE',
+      }),
+    }),
+  })
+})
+
+export const {
+  useAddTextMutation,
+  useDeleteTextMutation,
+  useSingleTextQuery,
+  useUpdateTextMutation,
+  useAllTextQuery,
+  useCountWordsQuery,
+  useCountCharactersQuery,
+  useCountParagraphsQuery,
+  useCountSentencesQuery,
+  useGetLongestWordsQuery
+} = analyzerApi;
